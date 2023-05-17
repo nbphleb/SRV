5de2fafc984ea81ce4585003de0e7b517344bd1e
nbphleb <127736277+nbphleb@users.noreply.github.com>
2023-05-17 12:22:32 +0300

    Update check.c

--- lab1/check.c
+++ lab1/check.c
@@ -1,17 +1,18 @@
-#include "getname.h"
+#include <stdio.h>
 #include <string.h>
+#include "getname.h"
 
-int main()
+int main(void)
 {
 	char truename[] = "Asonova Svetlana Borisovna I992";
 	if (strcmp(truename, getName()) == 0)
 	{
-		printf("Name is correct!");
+		printf("Name is correct \n");
 		return 0;
 	}
 	else
 	{
-		printf("Name is incorrect!");
+		printf("Name is incorrect \n");
 		return 1;
 	}
 }
